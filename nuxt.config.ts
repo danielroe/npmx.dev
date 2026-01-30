b50306bb0437872ed7c1a4a5b4388ca1b306c428
userquin <userquin@gmail.com>
2026-01-30 06:28:29 +0100

    chore: add pwa icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -161,6 +161,30 @@ export default defineNuxtConfig({
       description: 'A fast, modern browser for the npm registry',
       theme_color: '#0a0a0a',
       background_color: '#0a0a0a',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
     },
   },
 
