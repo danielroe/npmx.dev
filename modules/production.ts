d50623b1a46e92257b3c90a1a71b2dc78faa079c
Philippe Serhal <philippe.serhal@gmail.com>
2026-02-02 22:21:19 -0500

    chore: add debug logs and try fixing GitHub Actions detection

--- modules/production.ts
+++ modules/production.ts
@@ -9,7 +9,19 @@ export default defineNuxtModule({
   setup() {
     const nuxt = useNuxt()
 
-    if (isCI && provider !== 'github_actions') {
+    const isGitHubActions = provider === 'github_actions' || !!process.env.GITHUB_ACTIONS
+    const isTest = process.env.NODE_ENV === 'test'
+
+    console.log('[prod-env]', {
+      isCI,
+      provider,
+      isGitHubActions,
+      isTest,
+      GITHUB_ACTIONS: process.env.GITHUB_ACTIONS,
+    })
+
+    if (isCI && !isGitHubActions && !isTest) {
+      console.log('[prod-env] Enabling hydration debugging')
       nuxt.options.debug = defu(nuxt.options.debug, { hydration: true })
     }
   },
