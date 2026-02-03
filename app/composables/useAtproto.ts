240eccb4458d3b4924347d3ae16032c130c5f536
Daniel Roe <daniel@roe.dev>
2026-02-03 07:46:14 +0000

    chore: refresh session after mounting components

--- app/composables/useAtproto.ts
+++ app/composables/useAtproto.ts
@@ -4,10 +4,14 @@ export function useAtproto() {
   const {
     data: user,
     pending,
+    refresh,
     clear,
   } = useFetch<UserSession | null>('/api/auth/session', {
     server: false,
+    immediate: false,
   })
+
+  onNuxtReady(() => refresh())
 
   async function logout() {
     await $fetch('/api/auth/session', {
