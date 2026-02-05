8f52a60ed37565e6d8f6298a111d0b6a66ead885
James Garbutt <43081j@users.noreply.github.com>
2026-02-05 13:57:55 +0000

    chore: make this sketchy file sketchier

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -4,7 +4,12 @@
  * It's "stupid by design" so it could work in minimal Node.js environments.
  */
 
+import { cpSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
 import { spawnSync } from 'node:child_process'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+
+const projectDir = process.cwd()
 
 function getDependencyVersion(dependencyName: string): string {
   const result = spawnSync('npm', ['pkg', 'get', `devDependencies.${dependencyName}`], {
@@ -18,8 +23,8 @@ function getDependencyVersion(dependencyName: string): string {
   return JSON.parse(result.stdout)
 }
 
-function runCommand(command: string, args: string[]) {
-  const result = spawnSync(command, args, { stdio: 'inherit' })
+function runCommand(command: string, args: string[], cwd?: string) {
+  const result = spawnSync(command, args, { stdio: 'inherit', cwd })
 
   if (result.status) {
     throw new Error(`Command failed: ${command} ${args.join(' ')}`)
@@ -30,7 +35,21 @@ const oxlintVersion = getDependencyVersion('oxlint')
 const oxfmtVersion = getDependencyVersion('oxfmt')
 const e18eVersion = getDependencyVersion('@e18e/eslint-plugin')
 
-// Install globally so we can also install peers
-runCommand('pnpm', ['i', '-g', `@e18e/eslint-plugin@${e18eVersion}`, `oxlint@${oxlintVersion}`])
-runCommand('pnpm', ['exec', 'oxlint'])
+// Create a temp dir because:
+// 1. oxlint seems to try to resolve plugins from the dir of the config file
+// 2. pnpx/pnpm dlx doesn't have a clue about peers (plugins here), so doesn't have a way to install them
+const tempDir = mkdtempSync(join(tmpdir(), 'oxlint-'))
+try {
+  writeFileSync(join(tempDir, 'package.json'), '{"name": "temp", "version": "1.0.0"}')
+  cpSync('.oxlintrc.json', join(tempDir, '.oxlintrc.json'))
+  runCommand(
+    'pnpm',
+    ['install', '-D', `oxlint@${oxlintVersion}`, `@e18e/eslint-plugin@${e18eVersion}`],
+    tempDir,
+  )
+  runCommand('pnpm', ['exec', 'oxlint', '-c', join(tempDir, '.oxlintrc.json'), projectDir], tempDir)
+} finally {
+  rmSync(tempDir, { recursive: true, force: true })
+}
+
 runCommand('pnpx', [`oxfmt@${oxfmtVersion}`, '--check'])
