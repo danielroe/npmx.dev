18d6dfc277427d9176028ee4d13ade56894c4c9e
Sybren W <sybren.willemot@gmail.com>
2026-02-07 22:51:59 +0100

    refactor: use new xs text fontsize classes

--- app/components/Package/DownloadAnalytics.vue
+++ app/components/Package/DownloadAnalytics.vue
@@ -915,7 +915,7 @@ const chartConfig = computed(() => {
                   </svg>
                 </div>
 
-                <span class="text-xxs uppercase tracking-wide text-[var(--fg)]/70 truncate">
+                <span class="text-3xs uppercase tracking-wide text-[var(--fg)]/70 truncate">
                   ${label}
                 </span>
 
@@ -962,7 +962,7 @@ const chartConfig = computed(() => {
         <div class="flex flex-col gap-1 sm:shrink-0">
           <label
             for="granularity"
-            class="text-xxs font-mono text-fg-subtle tracking-wide uppercase"
+            class="text-3xs font-mono text-fg-subtle tracking-wide uppercase"
           >
             {{ $t('package.downloads.granularity') }}
           </label>
@@ -988,7 +988,7 @@ const chartConfig = computed(() => {
           <div class="flex flex-col gap-1">
             <label
               for="startDate"
-              class="text-xxs font-mono text-fg-subtle tracking-wide uppercase"
+              class="text-3xs font-mono text-fg-subtle tracking-wide uppercase"
             >
               {{ $t('package.downloads.start_date') }}
             </label>
@@ -1007,7 +1007,7 @@ const chartConfig = computed(() => {
           </div>
 
           <div class="flex flex-col gap-1">
-            <label for="endDate" class="text-xxs font-mono text-fg-subtle tracking-wide uppercase">
+            <label for="endDate" class="text-3xs font-mono text-fg-subtle tracking-wide uppercase">
               {{ $t('package.downloads.end_date') }}
             </label>
             <div
