23dcde5adf840de3bda311ac3361045ed71f675c
Marcus Blättermann <marcus@essenmitsosse.de>
2026-02-10 10:11:15 +0100

    fix: function call

--- app/pages/package-code/[[org]]/[packageName]/v/[version]/[...filePath].vue
+++ app/pages/package-code/[[org]]/[packageName]/v/[version]/[...filePath].vue
@@ -249,7 +249,7 @@ function copyPermalinkUrl() {
 }
 
 // Canonical URL for this code page
-const canonicalUrl = computed(() => `https://npmx.dev${getCodeUrl()}`)
+const canonicalUrl = computed(() => `https://npmx.dev${getCodeUrl(route.params)}`)
 
 // Toggle markdown view mode
 const markdownViewModes = [
