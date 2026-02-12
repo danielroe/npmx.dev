eb4e22b75d5593bab8ccdcc1fb6f7d5bc4b94c17
Daniel Roe <daniel@roe.dev>
2026-02-12 16:08:56 +0000

    fix: update to refresh on 1 + ignore `meta-refresh` rule

--- server/plugins/fallback-ui.ts
+++ server/plugins/fallback-ui.ts
@@ -1,7 +1,9 @@
 export default defineNitroPlugin(nitroApp => {
-  nitroApp.hooks.hook('render:html', (html, { event }) => {
-    if (event.path.includes('/200.html')) {
-      html.head.push('<noscript><meta http-equiv="refresh" content="0"></noscript>')
-    }
-  })
+  if (import.meta.prerender) {
+    nitroApp.hooks.hook('render:html', (html, { event }) => {
+      if (event.path === '/200.html') {
+        html.head.push('<noscript><meta http-equiv="refresh" content="1"></noscript>')
+      }
+    })
+  }
 })
