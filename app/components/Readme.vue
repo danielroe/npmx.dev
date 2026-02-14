a75dabca38c0ca02dd9883412195b3ac2c41e16d
jellydeck <91427591+jellydeck@users.noreply.github.com>
2026-02-14 10:36:48 +0530

    close the brackets

--- app/components/Readme.vue
+++ app/components/Readme.vue
@@ -150,11 +150,12 @@ function handleClick(event: MouseEvent) {
   @apply inline i-lucide:external-link rtl-flip ms-1 opacity-50;
 }
 
- .readme :deep(a[target='_blank']:has(img)) {
- /* Hide icon for links containing images */
- &::after {
-   display: none;
- }
+.readme :deep(a[target='_blank']:has(img)) {
+  /* Hide icon for links containing images */
+  &::after {
+    display: none;
+  }
+}
 
 .readme :deep(a[href^='#']::after) {
   /* I don't know what kind of sorcery this is, but it ensures this icon can't wrap to a new line on its own. */
