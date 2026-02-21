5694bc5c793226a8d895ec7618dd8fa74f3f060d
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-02-21 18:17:47 +0900

    test: ensure to have exact package url `/package/nuxt`

--- test/e2e/search-feeling-lucky.spec.ts
+++ test/e2e/search-feeling-lucky.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from './test-utils'
 test.describe('Search "I\'m Feeling Lucky" Redirect', () => {
   test('direct URL access with "!" should redirect to package', async ({ page, goto }) => {
     await goto('/search?q=nuxt!', { waitUntil: 'hydration' })
-    await expect(page).toHaveURL(/\/package\/nuxt/, { timeout: 15000 })
+    await expect(page).toHaveURL(/\/package\/nuxt$/, { timeout: 15000 })
   })
 
   test('normal search query (without "!") should not redirect', async ({ page, goto }) => {
