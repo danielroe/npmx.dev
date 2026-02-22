28c6ff0ef4d02c556e5a8705248ae5b7ea8cb495
Vordgi <sasha2822222@gmail.com>
2026-02-22 20:02:37 +0000

    chore: improve trailing slash middleware

--- app/middleware/trailing-slash.global.ts
+++ app/middleware/trailing-slash.global.ts
@@ -8,6 +8,13 @@
  * - /docs/getting-started/?query=value → /docs/getting-started?query=value
  */
 export default defineNuxtRouteMiddleware(to => {
+  if (import.meta.server) {
+    const event = useRequestEvent()
+    const url = event?.node.req.originalUrl || event?.node.req.url || ''
+
+    if (url.includes('_payload')) return
+  }
+
   if (to.path !== '' && !to.path.endsWith('/')) {
     return navigateTo(
       {
