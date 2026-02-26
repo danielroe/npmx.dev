aecd8416dd11d4cf395f3ac0f390fbd95d714a19
Sébastien Chopin <atinux@gmail.com>
2026-02-26 15:46:51 +0100

    knip fix

--- knip.ts
+++ knip.ts
@@ -61,7 +61,14 @@ const config: KnipConfig = {
     },
     'docs': {
       entry: ['app/**/*.{ts,vue,css}'],
-      ignoreDependencies: ['docus', 'better-sqlite3', '@nuxtjs/mdc', 'nuxt!'],
+      ignoreDependencies: [
+        'docus',
+        'better-sqlite3',
+        '@nuxtjs/mdc',
+        'nuxt!',
+        '@nuxt/ui',
+        'tailwindcss',
+      ],
     },
   },
 }
