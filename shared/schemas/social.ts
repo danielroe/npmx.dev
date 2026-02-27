7bde6b72b1f49a368315de58c8780aefe3ea6bad
Daniel Roe <daniel@roe.dev>
2026-02-27 12:38:47 +0000

    fix: add validation constraints to ProfileEditBodySchema

--- shared/schemas/social.ts
+++ shared/schemas/social.ts
@@ -12,9 +12,9 @@ export type PackageLikeBody = v.InferOutput<typeof PackageLikeBodySchema>
 
 // TODO: add 'avatar'
 export const ProfileEditBodySchema = v.object({
-  displayName: v.string(),
-  website: v.optional(v.string()),
-  description: v.optional(v.string()),
+  displayName: v.pipe(v.string(), v.maxLength(640)),
+  website: v.optional(v.pipe(v.string(), v.url())),
+  description: v.optional(v.pipe(v.string(), v.maxLength(2560))),
 })
 
 export type ProfileEditBody = v.InferOutput<typeof ProfileEditBodySchema>
