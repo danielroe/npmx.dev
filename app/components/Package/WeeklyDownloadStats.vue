597a6e7952ad92b5d197e5701fe7f56419f8375a
Vordgi <sasha2822222@gmail.com>
2026-03-02 16:07:38 +0000

    fix: update spark line height

--- app/components/Package/WeeklyDownloadStats.vue
+++ app/components/Package/WeeklyDownloadStats.vue
@@ -300,7 +300,7 @@ const config = computed<VueUiSparklineConfig>(() => {
         <span v-else-if="isLoadingWeeklyDownloads" class="min-w-6 min-h-6 -m-1 p-1" />
       </template>
 
-      <div class="w-full overflow-hidden h-[76px] motion-safe:h-[calc(92px+0.75rem)]">
+      <div class="w-full overflow-hidden h-[76px]">
         <template v-if="isLoadingWeeklyDownloads || hasWeeklyDownloads">
           <ClientOnly>
             <VueUiSparkline class="w-full max-w-xs" :dataset :config>
